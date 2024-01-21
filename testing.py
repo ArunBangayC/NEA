ad5f49a97f12b9ee90b88e43362750040799431f
Arun <arun.bangay@challoners.org>
2024-01-21 17:38:21 +0000

    much better hashing function

--- testing.py
+++ testing.py
@@ -1,19 +1,14 @@
-def hashFunction(username,password):
+def hashFunction(password):
     #sumOfPassword is the sum of all the squared unicode numerical codes from the password
     sumOfPassword = 0
     password  = list(password)
     for i in range(len(password)):
         sumOfPassword += (ord(password[i]))**2
-    sumOfPassword = list(str(sumOfPassword))
-    firstAndLastDigit = int(sumOfPassword[0] + sumOfPassword[-1])
-    middleDigits = int("".join(sumOfPassword[1:-1]))
+    listOfSumOfPassword = list(str(sumOfPassword))
+    firstAndLastDigit = int(listOfSumOfPassword[0] + listOfSumOfPassword[-1])
+    middleDigits = int("".join(listOfSumOfPassword[1:-1]))
     productOfSquares = ((firstAndLastDigit**2)*(middleDigits**2))**2
-    #sumOfUsername is the sum of all the unicode numerical codes from the username
-    sumOfUsername = 0
-    username = list(username)
-    for i in range(len(username)):
-        sumOfUsername += ord(username[i])
-    finalHash = hex(productOfSquares ^ sumOfUsername)
+    finalHash = hex(productOfSquares ^ sumOfPassword)
     return finalHash
 
-print(hashFunction("username","password"))
+print(hashFunction("bumbaclart"))
