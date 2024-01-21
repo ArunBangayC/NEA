89686a3ddca32eaa2e976564cf8d29728c0b541f
Arun <arun.bangay@challoners.org>
2024-01-21 17:29:53 +0000

    finished hashing algorithm

--- main.py
+++ main.py
@@ -85,16 +85,23 @@ def mainMenu():
         else:
             print("\n Oops, looks like you didn't enter L or A... Try again!")
     
-def hashFunction(password):
-    #sumOfUnicodes is the sum of all the squared unicode numerical codes
-    sumOfUnicodes = 0
+def hashFunction(username,password):
+    #sumOfPassword is the sum of all the squared unicode numerical codes from the password
+    sumOfPassword = 0
     password  = list(password)
     for i in range(len(password)):
-        sumOfUnicodes += (ord(password[i]))**2
-    sumOfUnicodes = list(str(sumOfUnicodes))
-    firstAndLastDigit = int(sumOfUnicodes[0] + sumOfUnicodes[-1])
-    middleDigits = int("".join(sumOfUnicodes[1:-1]))
-    return (firstAndLastDigit**2)*(middleDigits**2)
+        sumOfPassword += (ord(password[i]))**2
+    sumOfPassword = list(str(sumOfPassword))
+    firstAndLastDigit = int(sumOfPassword[0] + sumOfPassword[-1])
+    middleDigits = int("".join(sumOfPassword[1:-1]))
+    productOfSquares = ((firstAndLastDigit**2)*(middleDigits**2))**2
+    #sumOfUsername is the sum of all the unicode numerical codes from the username
+    sumOfUsername = 0
+    username = list(username)
+    for i in range(len(username)):
+        sumOfUsername += ord(username[i])
+    finalHash = hex(productOfSquares ^ sumOfUsername)
+    return finalHash
 
 mainMenu()
 connectionToDatabase.commit()
--- testing.py
+++ testing.py
@@ -1,8 +1,19 @@
-import sqlite3
-connectionToDatabase = sqlite3.connect("/home/arun/Documents/NEA/mainDatabase.db")
-cursor = connectionToDatabase.cursor()
+def hashFunction(username,password):
+    #sumOfPassword is the sum of all the squared unicode numerical codes from the password
+    sumOfPassword = 0
+    password  = list(password)
+    for i in range(len(password)):
+        sumOfPassword += (ord(password[i]))**2
+    sumOfPassword = list(str(sumOfPassword))
+    firstAndLastDigit = int(sumOfPassword[0] + sumOfPassword[-1])
+    middleDigits = int("".join(sumOfPassword[1:-1]))
+    productOfSquares = ((firstAndLastDigit**2)*(middleDigits**2))**2
+    #sumOfUsername is the sum of all the unicode numerical codes from the username
+    sumOfUsername = 0
+    username = list(username)
+    for i in range(len(username)):
+        sumOfUsername += ord(username[i])
+    finalHash = hex(productOfSquares ^ sumOfUsername)
+    return finalHash
 
-query = "DROP TABLE IF EXISTS Users"
-
-cursor.execute(query)
-connectionToDatabase.close()
+print(hashFunction("username","password"))
