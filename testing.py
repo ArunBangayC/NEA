654b425d0e92c1db248bc9a60813aa9d62bc22c1
Arun <arun.bangay@challoners.org>
2024-02-17 19:58:02 +0000

    good random generation function

--- testing.py
+++
@@ -1,9 +0,0 @@
-def matrixOperableList(parameter):
-    separatedList = []
-    for i in range(0,len(parameter),4):
-        miniList = parameter[i:i+4]
-        separatedList.append(miniList)
-    return separatedList
-
-lis = [1,2,3,4,5,6,7,8,9,10]
-print(matrixOperableList(lis))
